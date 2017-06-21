a8f595e20ce966cd623f4c3f93acc8244ab409d4
select0 <495596154@qq.com>
2017-06-21 11:33:40 +0800

    2017-6-21

--- test.php
+++ test.php
@@ -1,6 +1,29 @@
 
 <?php
 	header("Content-type:text/html;charset=utf-8");
+	date_default_timezone_set("PRC");
 	echo "test";
 	echo "我的歌";
+	echo "<br/>";
+	/*
+	$a=1111;
+	$b=2222;
+	$b=explode(",",$a.",".$b.","."333333");
+	var_dump($b);
+	
+	$a=11111;
+	$b=22222;
+	$c=33333;
+	$c=$a.",".$b.",".$c;
+	$c=explode(",",$c);
+	var_dump($c);
+	
+	$int=strtotime("2011-5-12 21:06");
+	echo date("y年m月d日 H时i分",$int);
+	
+	$conn=mysql_connect("localhost","root","root");
+	mysql_select_db("dbname",$conn);
+	$pdo=new PDO(mysql:host="localhost";dbname="dbname","root","root");
+	*/
+	
 ?>
